6452719952d83738f45194deb8865290304e4612
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2018-02-24 15:12:54 +0300

    quadratic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function solveEquation(equation) {
+    let arr = equation.split(' ');
+    
+    let a = 0,
+        b = 0,
+        c = 0;
+    a = Number (arr[0]);
+ 
+    if (arr[3] == '-') {
+        b = Number (arr[3] + arr[4]);
+    } else {
+        b = Number (arr[4]);
+    }
+
+    if (arr[7] == '-') {
+        c = Number (arr[7] + arr[8]);
+    } else {
+        c = Number (arr[8]);
+    }
+
+    let d = Math.pow(b, 2) - (4 * a *c);
+        
+    if (d > 0) {
+        let x = ((-b) + Math.sqrt(d)) / (2 * a),
+            y = ((-b) - Math.sqrt(d)) / (2 * a),
+            newArr = [Math.round(x), Math.round(y)];
+            if (a > 0) {
+                return newArr.reverse();
+            }
+            return newArr;
+    }
+    
   // your implementation
 }
